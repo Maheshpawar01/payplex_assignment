072a6aab3c288ec5a5a734d42f14c1afe62ae80a
Mahesh Pawar <mp002995@gmail.com>
2025-11-14 15:04:01 +0530

    update the frontend url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // frontend url
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://payplex-assignment-phi.vercel.app/login',
+  origin: process.env.CLIENT_URL || 'https://payplex-assignment-phi.vercel.app',
   credentials: true
 }));
 app.use(express.json());
